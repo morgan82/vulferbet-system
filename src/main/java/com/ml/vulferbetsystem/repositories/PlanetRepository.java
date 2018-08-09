fda74472a3453126a3222507280d3c082b3ac3ce
leonardo.morganti <leonardo.morganti@globallogic.com>
2018-08-09 14:14:51 -0300

    *cleanups

--- src/main/java/com/ml/vulferbetsystem/repositories/PlanetRepository.java
+++ src/main/java/com/ml/vulferbetsystem/repositories/PlanetRepository.java
@@ -3,7 +3,9 @@ package com.ml.vulferbetsystem.repositories;
 import com.ml.vulferbetsystem.domain.Planet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlanetRepository extends JpaRepository<Planet,Long> {
     @Query(value = "SELECT * FROM WEATHER WHERE WEATHER_DATE = ((SELECT min(WEATHER_DATE) FROM WEATHER )+(?1-1))", nativeQuery = true)
     Planet findByWeatherDate(int days);
